6eb378ea3db0fc7fdf0de3629c6854a2291e822e
Eric Dietrich <edietrich@montanafreepress.org>
2025-06-02 15:40:24 -0600

    Reformat lege SVGs for export compatability

--- process-atlas/legislative-districts/merge-files/convert-to-png.sh
+++ process-atlas/legislative-districts/merge-files/convert-to-png.sh
@@ -2,8 +2,14 @@
 # Convert all images from SVG to PNG with imagemagick
 # Needs to be run from this directory for links to work correctly
 # sh convert-to-png.sh
-# mogrify -format png *.svg
+
+# NOTE: SVGs out of Adobe Illustrator need to be saved w/ Advanced Options >> CSS Properties set to "Presentation Attributes"
+# Otherwise AI trying to store styling in CSS classes confuses imagemagick
+
+mogrify -format png *.svg
+mv *.png ../merged-temp/
 # This isn't working as of 5/20/2025
 
 # Convert single file by name
-mogrify -format png ./HD-1-test.svg
+# magick -format png ./HD-1-test.svg 
+# magick HD-1-test.svg HD-1-test.png
