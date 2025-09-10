0a5513f1c78b29f30262642a49d11b6ff71214b2
Eric Dietrich <edietrich@montanafreepress.org>
2025-09-10 11:07:15 -0600

    Correct winding direction

--- scripts/clean/clean-mt-state-boundary.sh
+++ scripts/clean/clean-mt-state-boundary.sh
@@ -7,13 +7,15 @@
 layer_slug="mt-state-boundary"
 
 # initial cleanup at full-scale
+# gj2008 flag should ensure geojson are wound in the correct direction
+
 mapshaper "./data/raw/msl/"${layer_slug}".zip" \
     -proj wgs84 \
-    -o data/processed/original-resolution/${layer_slug}.geojson
+    -o gj2008 data/processed/original-resolution/${layer_slug}.geojson
 
 # Resolutions in meters
 for scale in 1000 100 10 1; do
     mapshaper "data/processed/original-resolution/${layer_slug}.geojson" \
         -simplify keep-shapes interval=${scale} \
-        -o precision=0.00001 data/processed/${scale}m-resolution/${layer_slug}-${scale}m.geojson
+        -o gj2008 precision=0.00001 data/processed/${scale}m-resolution/${layer_slug}-${scale}m.geojson
 done
