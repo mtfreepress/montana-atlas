01efe4bf9ef74973f375b67d2d1b786cc6f9caf8
Jacob Olness <jolness1@gmail.com>
2025-10-07 10:00:52 -0600

    tweaks

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,12 +1,9 @@
+#!/usr/bin/env bash
+
 # Common environment helpers for repo scripts
 # Export MAPSHAPER pointing to local node_modules binary when available,
 # otherwise fall back to global `mapshaper` in PATH.
 
-#!/usr/bin/env bash
-
-# Do NOT set errexit/pipefail/etc here because this file is meant to be
-# sourced by other scripts; we don't want to change their shell options.
-# Respect an already-set MAPSHAPER so callers can override.
 if [ -z "${MAPSHAPER:-}" ]; then
   MAPSHAPER_CANDIDATE="./node_modules/.bin/mapshaper"
   if [ -x "$MAPSHAPER_CANDIDATE" ]; then
